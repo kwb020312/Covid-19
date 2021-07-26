e879e1e09c2cca7bb07604d6228e4c14a36d2cce
kwb020312 <kwb020312@naver.com>
2021-07-26 14:33:02 +0900

    React Chart적용

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 
 const Contents = () => {
+  const [confirmedData, setConfirmedData] = useState({
+    labels: ["1월", "2월", "3월"],
+    datasets: [
+      {
+        label: "국내 누적 확진자",
+        backgroundColor: "salmon",
+        fill: true,
+        data: [10, 5, 3],
+      },
+    ],
+  });
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get(`https://api.covid19api.com/country/kr`);
+      makeData(res.data);
+    };
+    const makeData = (items) => {
+      const arr = items.reduce((acc, cur) => {
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        return acc;
+      }, []);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <section>
       <h2>국내 코로나 현황</h2>
-      <div className="contents"></div>
+      <div className="contents">
+        <div>
+          <Bar
+            data={confirmedData}
+            options={{
+              title: {
+                display: true,
+                text: "누적 확진자 추이",
+                fontSize: 16,
+              },
+              legend: { display: true, position: "bottom" },
+            }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
